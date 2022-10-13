44e8b6ce8b53f339bc68519e701dd0dcc600c307
Adrian Orioki <adrianorioki@gmail.com>
2022-10-13 13:38:22 +0300

    Urls

--- web_app/hello/urls.py
+++ web_app/hello/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from web_app.hello import views
 
 urlpatterns = (
-    path("", views.index, name = "index")
+    path("", views.index, name = "index"),
+    path("brian", views.brian, name = "brian"),
+    path("david", views.david, name = "david")
 )
--- web_app/hello/views.py
+++ web_app/hello/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("Hello!")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
